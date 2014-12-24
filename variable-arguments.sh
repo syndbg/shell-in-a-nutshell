dc669e057169bf421aca8035ee7e95d5c3093669
syndbg <syndbe@gmail.com>
2014-12-24 22:35:13 +0200

    Added variable arguments examples

---
+++ variable-arguments.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+# Positional arguments
+echo "It takes one: $1"
+echo "And two: $2"
+echo "To make: $3"
+
+# All arguments
+echo "Why can't I hold all these $@"
+
+# N of arguments passed
+echo "If it's the Psychic Network why do they need an arguments number? $#"
+
+# Script name
+echo "Hello, my name is $0. You killed my father, prepare to die!"
+
+# Process ID
+echo "The T-$$ can't form complex machines."
+
+# Exit Code
+echo "$?"
