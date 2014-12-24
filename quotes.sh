4dda11b3a6d5e777dfb73f32e78bbad20521ae3d
syndbg <syndbe@gmail.com>
2014-12-24 22:38:15 +0200

    Added comments for clarity in quotes.sh

--- quotes.sh
+++ quotes.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Variables are expanded
 echo 1 No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH.
-echo 2 'No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH.'
-echo 3 "No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH."
+echo 2 "No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH."
+
+
+# But not with single-quotes!
+echo 3 'No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH.'
 
 
 # As expected, you can escape characters just like in other languages with a backslash
