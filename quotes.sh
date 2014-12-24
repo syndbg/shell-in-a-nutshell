a229f7b6b461780ca74bb70bdd16ceb52d5b0165
syndbg <syndbe@gmail.com>
2014-12-24 18:30:25 +0200

    Added escaping

--- quotes.sh
+++ quotes.sh
@@ -3,3 +3,7 @@
 echo 1 No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH.
 echo 2 'No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH.'
 echo 3 "No one saves us but ourselves. No one can and no one may. We ourselves must walk the $PATH."
+
+
+# As expected, you can escape characters just like in other languages with a backslash
+echo 'You cannot \tescape\nthe responsibility of tomorrow by evading it today.'
